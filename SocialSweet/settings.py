5104b9d72882f5ef45b1488e436d1dc5bc08f238
akash <akashbachhar2001@gmail.com>
2021-09-02 13:03:02 +0530

    change settings

--- SocialSweet/settings.py
+++ SocialSweet/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-gv3nn6&*z8b&1b4vev6v&5(++%(*cn$o1#s^fs#twr=2v^$po*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
