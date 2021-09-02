3da289d6fa712030402f7ecac94c137fa5101343
akash <akashbachhar2001@gmail.com>
2021-09-02 13:17:53 +0530

    change settins

--- SocialSweet/settings.py
+++ SocialSweet/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 import django_heroku
 from django.contrib.messages import constants as messages
